48453922be05a17c9a1c42d4f13af4f1d5cc1c1c
Yasser-Alnajjar <yasseralnajjar@outlook.sa>
2023-08-11 04:16:16 +0300

    change url server

--- src/redux/apiSlice.js
+++ src/redux/apiSlice.js
@@ -2,10 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials, logOut } from "./auth/authSlice";
 import { URL_API } from "../utils";
 const baseQuery = fetchBaseQuery({
-  baseUrl:
-    process.env.NODE_ENV === "production"
-      ? process.env.NEXT_PUBLIC_Server_Url
-      : process.env.NEXT_PUBLIC_Server_Url_Local,
+  baseUrl: process.env.NEXT_PUBLIC_Server_Url,
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
